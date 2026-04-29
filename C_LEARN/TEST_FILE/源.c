1ab4bf21d0cd0cf42e7de972d681e3466a3d6e70
Shmily219 <1589671067@qq.com>
2026-04-29 11:02:41 +0800

    更新时间为2026-04-29 11:02:40

--- C_LEARN/TEST_FILE/源.c
+++ C_LEARN/TEST_FILE/源.c
@@ -2,6 +2,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> //io功能库
 #include <string.h>
+#include <windows.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 //计算阶乘
 int Count_jiecheng(int i)
@@ -15,20 +19,52 @@ int Count_jiecheng(int i)
     if (i == 0)
         return 1;
     return i * Count_jiecheng(i - 1);
- };
+};
 
 //计算斐波那契的第n项
 int FBNQ(int i) {
     if (i == 1 || i == 2) {
         return 1;
     }
-        return FBNQ(i - 1) + FBNQ(i - 2);
-    };
+    return FBNQ(i - 1) + FBNQ(i - 2);
+};
 
-    int main() {
+//生成随机数
+int Create_random() {
+    int low = 10;
+    int hight = 50;
+    srand(time(NULL));
+    int seed = rand()%100+1;  //生成一个1-100的随机数
+    //printf("%d\n", seed);
+    srand(rand() % 100 + 1);
+    int a = rand() % (hight - low + 1) + low;
+    return a;
+}
 
-    printf("%d\n",5/2);
-    printf("%d\n",5/2 + 1);
+int main() {
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", Create_random());
+        Sleep(1000);
+    }
+
+
+
+    ////字符串截取,
+    //char arr1[] = "welcome to Xihua!";
+    //char arr2[] = "#################";
+    //int left = 0;
+    //int right = strlen(arr1) - 1; //字符串末尾有一个\0转义,要减去
+    //while (left<=right){
+    //    //for (int i = 0; i <= left;i++) {
+    //    arr2[left] = arr1[left];
+    //    arr2[right] = arr1[right];
+    //    printf("%s\n",arr2);
+    //    Sleep(1000);
+    //    system("cls");
+    //    //}
+    //    left++;
+    //    right--;
+    //}
 
     //计算n的阶乘
     //printf("输入你准备计算阶乘的数\n");
@@ -75,3 +111,5 @@ int FBNQ(int i) {
     ////printf("日期为%02d\n", D);
     return 0;
 }
+
+
